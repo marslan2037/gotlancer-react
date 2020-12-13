9d5d3f8b1213777cac956452737ef2d5d4aa5f05
c0destudent <abdulbasit_9988@yahoo.com>
2020-12-13 09:53:05 -0800

    code difference

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Footer from "./Footer";
-import BrowseOffers from "./BrowseOffers";
+import BrowseOffer from "./BrowseOffer";
 import BuyerDashboard from "./BuyerDashboard";
 import FreelancerDashboard from "./FreelancerDashboard";
 
@@ -11,11 +11,19 @@ export default class Home extends React.Component {
     render() {
         return (
             <>
-                <div className="main-container">
+                {/* <div className="main-container">
                     <Header />
-                    <BrowseOffers />
+                    <FreelancerDashboard/>
                     <Footer />
-                </div>
+                </div> */}
+                <Router>
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route path='/browse-offer' component={Contact} />
+                        <Route path='/buyer-dashboard' component={About} />
+                        <Route path='/free' component={About} />
+                    </Switch>
+                </Router>
             </>
         )
     }
