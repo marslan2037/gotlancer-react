c81da331c30ef09c733094da36f22f582a21436d
Muhammad Arslan <m.arslan2037@gmail.com>
2020-12-16 21:36:19 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/Header";
 import BrowseOffers from "./components/BrowseOffers";
 import BuyerDashboard from "./components/BuyerDashboard";
 import FreelancerDashboard from "./components/FreelancerDashboard";
-import BasicTable from "./components/MaterialTest";
+import CustomPaginationActionsTable from "./components/MaterialTest";
 
 function App() {
     return (
@@ -21,7 +21,7 @@ function App() {
                         <Route path='/browse-offers' component={BrowseOffers} />
                         <Route path='/buyer-dashboard' component={BuyerDashboard} />
                         <Route path='/freelancer-dashboard' component={FreelancerDashboard} />
-                        <Route path='/basic-table' component={BasicTable} />
+                        <Route path='/basic-table' component={CustomPaginationActionsTable} />
                     </Switch>
                     
                     <Footer />
